98c364839c7aae54bb73b757410994572317cc7a
Jason Lin <linjason920523@gmail.com>
2020-03-17 01:04:22 +0800

    finish first step of lisp_js native

--- src/lisp_js.js
+++ src/lisp_js.js
@@ -1,5 +1,60 @@
 var lisp_lib = {
-  if: (args)=>{
-    
+  if: (args) => {
+
   }
-}
+};
+
+(() => {
+  function onload() {
+    fetchLangConfig(()=>{
+      fetchLispScript();
+    })
+  }
+
+  function fetchLangConfig(then){
+    var count = 0;
+    var result = document.querySelectorAll('script[type="text/pec-lang"]');
+    console.log(result)
+    for (var index in result) {
+      if (result[index].src == undefined){
+        count ++;
+        if(count == result.length) then()
+        continue;
+      }
+
+      console.log(result[index].getAttribute("namespace"));
+
+      var url = result[index].src;
+      var namespace = result[index].namespace;
+      console.log(url);
+      fetch(url).then(function (response) {
+        response.text().then(function (code) {
+          var json = JSON.parse(code);
+          add_gfunc(json,namespace);
+
+          count++;
+
+          if(count == result.length)
+            then()
+        });
+      });
+    }
+  }
+
+  function fetchLispScript(then){
+    var result = document.querySelectorAll('script[type="text/pec-lisp"]')
+    for (var index in result) {
+      if (result[index].src == undefined) continue;
+      var url = result[index].src;
+      console.log(url);
+      fetch(url).then(function (response) {
+        response.text().then(function (code) {
+          var parsed = parenthesize(tokenize(code));
+          eval(run_lisp(parsed))
+        });
+      });
+    }
+  }
+
+  window.addEventListener("load", onload);
+})();
